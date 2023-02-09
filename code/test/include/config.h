a3594b62cdfa3735d6ff4df84aad0f09ddba009f
PMunyao <pmunyaow@gmail.com>
2023-02-09 14:05:07 +0300

    delete

--- code/test/include/config.h
+++
@@ -1,9 +0,0 @@
-// Variables for connecting to the WiFi network
-const char* ssid = "yourSSID";
-const char* password = "yourPassword";
-
-// Variables for connecting to the MQTT broker
-const char* mqttServer = "yourMQTTServerIPAddress";
-const int mqttPort = 1883;
-const char* mqttUsername = "yourMQTTUsername";
-const char* mqttPassword = "yourMQTTPassword";
